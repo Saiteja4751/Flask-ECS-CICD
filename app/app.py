19eb3f2bc049e2830e997345d1a0072066c7a73f
Saiteja4751 <sai.battu@atyeti.com>
2025-07-03 10:40:12 +0530

    update the second home

--- app/app.py
+++ app/app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Flask App Running on ECS with CI/CD!"
+@app.route('/')
+def secondHome():
+    return 'second home'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
