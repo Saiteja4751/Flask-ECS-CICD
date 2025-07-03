c190f0e0fe85b0ac176c9366551cb8b611bd579a
Saiteja4751 <sai.battu@atyeti.com>
2025-07-03 10:47:56 +0530

    adddddd

--- app/app.py
+++ app/app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Flask App Running on ECS with CI/CD!"
-@app.route('/')
+@app.route('/home')
 def secondHome():
-    return 'second home'
+    return "second home"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
